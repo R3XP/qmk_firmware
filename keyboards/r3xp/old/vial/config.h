192a3b4458f11c6d7d619a094b3c20cdd0e25203
Till Preikschat <till.preikschat@redclu.de>
2023-05-12 15:53:14 +0200

    WIP

---
+++ keyboards/r3xp/old/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VENDOR_ID 0x527E
+#define PRODUCT_ID 0x6877
+#define MANUFACTURER    R3XP
+#define PRODUCT         R3XP Howler42
+#define DESCRIPTION     R3XP Howler42
